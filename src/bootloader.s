2f8e645d84202070ceaf68501942415df9f47729
Michał Wilde <packerbat@gmail.com>
2018-04-08 00:58:18 +0200

    plfontslg loader moved to bootloader.prg

--- src/bootloader.s
+++ src/bootloader.s
@@ -1,10 +1,6 @@
 ;---------------------------------------------------------- 
 ; sekwencja inicjująca rozszerzenie Basica
 
-.segment "ZEROPAGE":zeropage
-SRCPTR:   .res 2
-DSTPTR:   .res 2
-
 .segment "CODE"
     .org $0801
 
@@ -18,26 +14,44 @@ DSTPTR:   .res 2
     .byte 0           ;end of basic line
     .word 0           ;wskaźnik na następną linię, $0000 oznacza, że jest to ostania linia
 
-    lda #$09
-    sta SRCPTR+1
-    lda #$90
-    sta DSTPTR+1
-    ldy #0
-    sty SRCPTR
-    sty DSTPTR
-    ldx #16             ;16 bloków po 256 bajtów
-:   lda (SRCPTR),y
-    sta (DSTPTR),y
-    dey
-    bne :-
-    inc SRCPTR+1
-    inc DSTPTR+1
-    dex
-    bne :-
+    lda #<loadingrom     ;napis powitalny
+    ldy #>loadingrom
+    jsr $AB1E     ;Output String
+
+    lda #7              ;długość nazwy pliku
+    ldx #<romfilename
+    ldy #>romfilename
+    jsr $FFBD           ;SETNAM
+    lda #1              ;numer strumienia
+    ldx #8              ;stacja dyskietek
+    ldy #0              ;secondary address, e.g. 15 to read the error channel
+    jsr $FFBA           ;SETLFS
+    lda #0              ;0=LOAD, 1=VERIFY
+    ldx #$00            ;only used if secondary address above = 0
+    ldy #$90
+    jsr $FFD5           ;LOAD
+
+    lda #<loadingfonts     ;napis powitalny
+    ldy #>loadingfonts
+    jsr $AB1E     ;Output String
+
+    lda #9              ;długość nazwy pliku
+    ldx #<fontfilename
+    ldy #>fontfilename
+    jsr $FFBD           ;SETNAM
+    lda #1              ;numer strumienia
+    ldx #8              ;stacja dyskietek
+    ldy #0              ;secondary address, e.g. 15 to read the error channel
+    jsr $FFBA           ;SETLFS
+    lda #0              ;0=LOAD, 1=VERIFY
+    ldx #$00            ;only used if secondary address above = 0
+    ldy #$A0
+    jsr $FFD5           ;LOAD
+
     jmp $9000     ;will init wpbasic and return to SYS(2069)
 
-    .res $08FE-*,0   ;wypełnienie zerami reszty segmentu, co za marnotrawstwo
-
-.segment "BINARY"
-    .incbin "wpbasic.prg"
+romfilename:   .byte "wpbasic"
+loadingrom:    .byte 13,"loading wpbasic...",0
+fontfilename:  .byte "plfontslg"
+loadingfonts:  .byte 13,"loading plfontslg...",0
 
--- src/init.s
+++ src/init.s
@@ -19,29 +19,6 @@
     lda #<TX1     ;napis powitalny
     ldy #>TX1
     jsr $AB1E     ;Output String
-    
-    
-    lda #<loadingfonts     ;napis powitalny
-    ldy #>loadingfonts
-    jsr $AB1E     ;Output String
-
-    lda #9              ;długość nazwy pliku
-    ldx #<fontfilename
-    ldy #>fontfilename
-    jsr $FFBD           ;SETNAM
-    lda #1              ;numer strumienia
-    ldx #8              ;stacja dyskietek
-    ldy #0              ;secondary address, e.g. 15 to read the error channel
-    jsr $FFBA           ;SETLFS
-    lda #0              ;0=LOAD, 1=VERIFY
-    ldx #$00            ;only used if secondary address above = 0
-    ldy #$A0
-    jsr $FFD5           ;LOAD
-
-    lda #<fontsloaded     ;napis powitalny
-    ldy #>fontsloaded
-    jsr $AB1E     ;Output String
-
 
     ldx #$0B          ;podmiana 6-ciu kolejnych werktorów BASICA
 :   lda NVC,x
@@ -71,6 +48,3 @@ NVC: .word  NOR,  $A483, TOKEN, LIST,  DOIT,   TEV
 TX1: .byte 155,147,"    ..... Wordprocessor BASIC .....", 13, 13
      .byte         "        34816 BASIC bytes free", 13, 0
 
-fontfilename:  .byte "plfontslg"
-loadingfonts:  .byte 13,"Loading PLFONTSLG...",0
-fontsloaded:   .byte " done", 0
